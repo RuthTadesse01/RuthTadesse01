ffda83d9316884371cd0e18f5ea29eb09af2aa9e
RuthTadesse01 <100129572+RuthTadesse01@users.noreply.github.com>
2022-03-06 17:55:24 +0100

    Initial commit

---
+++ src/MarsWeightCalculator.java
@@ -0,0 +1,35 @@
+//TASK 2
+
+public class MarsWeightCalculator {
+    public static void main(String[] args) {
+
+        float earthWeight;
+        float marsWeight ;
+        double marsWeightINDouble;
+        int marsWeightINInt;
+
+        earthWeight = 72;
+        marsWeight = (earthWeight =  0.38F);
+        System.out.println(earthWeight + "Fg on Earth is " + marsWeight + "Kg on Mars" );
+// assignment (compatible types) from float to double
+        marsWeightINDouble = marsWeight;
+        System.out.println ("Kilogram on Mars after converting to Double" +marsWeightINDouble);
+
+// printing a variable  limiting the length to 4 decimal places
+
+        System.out.printf("Kilogram in Mars displayed to four decimal palces: %.4f %n", marsWeightINDouble);
+
+//  a floating point type cast in integer
+
+        marsWeightINInt = (int)marsWeightINDouble;
+        System.out.println("Kilograms on Mars casted to integer" +marsWeightINInt);
+// casting an int type to char
+        char c = (char)marsWeightINInt;
+        System.out.println("the ASCII table equivalent  of marsWeightInInt is:" +c);
+//assign the value of performing some mathematical  operation on the char character to the new int variable
+        int exampleOfMathOnChar = c * c;
+        System.out.println("An example of a mathematical operation on the char type: "+ exampleOfMathOnChar);
+    }
+}
+
+
